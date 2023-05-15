a4e3f8156338c7d589e828d08999e5e80518f011
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-15 20:05:03 +0300

    added comments for Chrome6 different way Windows and Mac

--- lesson_test_1.py
+++ lesson_test_1.py
@@ -7,8 +7,8 @@ from selenium.webdriver.common.by import By
 # options = webdriver.ChromeOptions()
 # options.add_experimental_option("detach", True)
 # g = Service()
-# driver = webdriver.Chrome(options=options, service=g)
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome(options=options, service=g)  # this is and above MacOS
+driver = webdriver.Chrome()  # Windows
 # driver = webdriver.Firefox()
 # driver = webdriver.Safari()
 # driver = webdriver.Edge()
