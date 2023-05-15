3d0bfe875ff59976d11e86c389ac493f27c21e22
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-15 19:57:20 +0300

    scrolling and move to element

--- lesson_test_4.py
+++ lesson_test_4.py
@@ -1,14 +1,15 @@
 import datetime
 import time
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-# options = webdriver.ChromeOptions()
-# options.add_experimental_option("detach", True)
-# g = Service()
-# driver = webdriver.Chrome(options=options, service=g)
-driver = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+g = Service()
+driver = webdriver.Chrome(options=options, service=g)
+# driver = webdriver.Chrome()
 # driver = webdriver.Firefox()
 # driver = webdriver.Safari()
 # driver = webdriver.Edge()
@@ -28,14 +29,18 @@ print("Input password")
 button_login = driver.find_element(By.XPATH, "//input[@value='Login']")
 button_login.click()
 print("Click login button")
+time.sleep(5)
 
-driver.execute_script('window.scrollTo(X, Y)')
-# time.sleep(3)
-# now_date = datetime.datetime.utcnow().strftime("%Y.%m.%d.%H.%M.%S")
-# name_screenshot = 'screenshot' + now_date + '.png'
-# # driver.save_screenshot('/Users/vitalybesedin/PycharmProjects/python_selenium_first/screen/' + name_screenshot) # MacOS
+# driver.execute_script("window.scrollTo(0, 500)")
+action = ActionChains(driver)
+# red_tshirt = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-onesie']")
+red_tshirt = driver.find_element(By.XPATH, "//button[@id='add-to-cart-test.allthethings()-t-shirt-(red)']")
+action.move_to_element(red_tshirt).perform()
+
+time.sleep(3)
+now_date = datetime.datetime.utcnow().strftime("%Y.%m.%d.%H.%M.%S")
+name_screenshot = 'screenshot' + now_date + '.png'
+driver.save_screenshot('/Users/vitalybesedin/PycharmProjects/python_selenium_first/screen/' + name_screenshot) # MacOS
 # driver.save_screenshot('C:\\Users\\vit\\PycharmProjects\\python_selenium_first\\screen\\' + name_screenshot) # Windows
-
-
 
 time.sleep(5)
