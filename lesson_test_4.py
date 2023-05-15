0b9ee8b2cf9be95075b49fdc935d5259ef147203
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-15 19:17:25 +0300

    add scrolling

---
+++ lesson_test_4.py
@@ -0,0 +1,41 @@
+import datetime
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+# options = webdriver.ChromeOptions()
+# options.add_experimental_option("detach", True)
+# g = Service()
+# driver = webdriver.Chrome(options=options, service=g)
+driver = webdriver.Chrome()
+# driver = webdriver.Firefox()
+# driver = webdriver.Safari()
+# driver = webdriver.Edge()
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.maximize_window()
+
+login_standard_user = "standard_user"
+password_all = "secret_sauce"
+
+username = driver.find_element(By.XPATH, "//input[@data-test='username']")
+username.send_keys(login_standard_user)
+print("Input login")
+password = driver.find_element(By.CSS_SELECTOR, "#password")
+password.send_keys(password_all)
+print("Input password")
+button_login = driver.find_element(By.XPATH, "//input[@value='Login']")
+button_login.click()
+print("Click login button")
+
+driver.execute_script('window.scrollTo(X, Y)')
+# time.sleep(3)
+# now_date = datetime.datetime.utcnow().strftime("%Y.%m.%d.%H.%M.%S")
+# name_screenshot = 'screenshot' + now_date + '.png'
+# # driver.save_screenshot('/Users/vitalybesedin/PycharmProjects/python_selenium_first/screen/' + name_screenshot) # MacOS
+# driver.save_screenshot('C:\\Users\\vit\\PycharmProjects\\python_selenium_first\\screen\\' + name_screenshot) # Windows
+
+
+
+time.sleep(5)
