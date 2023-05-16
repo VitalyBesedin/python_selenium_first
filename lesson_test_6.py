d8d5d6827810f195000707b851b54781fbde6b13
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-16 20:56:36 +0300

    next step

--- lesson_test_6.py
+++ lesson_test_6.py
@@ -22,20 +22,31 @@ password_all = "secret_sauce"
 username = driver.find_element(By.XPATH, "//input[@data-test='username']")
 username.send_keys(login_standard_user)
 print("Input login")
+time.sleep(1)
 password = driver.find_element(By.CSS_SELECTOR, "#password")
 password.send_keys(password_all)
 print("Input password")
+time.sleep(1)
 button_login = driver.find_element(By.XPATH, "//input[@value='Login']")
 button_login.click()
 print("Click login button")
+time.sleep(1)
 
 """Info Product # 1"""
 product_1 = driver.find_element(By.XPATH, "//a[@id='item_4_title_link']")
 value_product_1 = product_1.text
 print(value_product_1)
+time.sleep(1)
 price_product_1 = driver.find_element(By.XPATH, "//*[@id='inventory_container']/div/div[1]/div[2]/div[2]/div")
 value_price_product_1 = price_product_1.text
 print(value_price_product_1)
-
+time.sleep(1)
+select_product_1 = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-backpack']")
+select_product_1.click()
+print("Select Product 1")
+time.sleep(1)
+cart = driver.find_element(By.XPATH, "//span[@class='shopping_cart_badge']")
+cart.click()
+print("Cart")
 
 time.sleep(5)
