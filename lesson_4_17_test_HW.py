3d047dc9fb614e80cd0d43e27a5bfba1c7d4ceb3
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-17 20:47:04 +0300

    add scrolling

--- lesson_4_17_test_HW.py
+++ lesson_4_17_test_HW.py
@@ -1,16 +1,14 @@
-import datetime
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 # options = webdriver.ChromeOptions()
 # options.add_experimental_option("detach", True)
 # g = Service()
 # driver = webdriver.Chrome(options=options, service=g)  # this is and above macOS
-# driver = webdriver.Chrome()  # Windows
+driver = webdriver.Chrome()  # Windows
 # driver = webdriver.Firefox()
-driver = webdriver.Safari()
+# driver = webdriver.Safari()
 # driver = webdriver.Edge()
 base_url = 'https://www.saucedemo.com/'
 driver.get(base_url)
