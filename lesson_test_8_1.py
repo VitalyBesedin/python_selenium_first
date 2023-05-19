8d298e059b12cf6d9e2d8b852e7eb876c955a8e2
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-19 17:10:09 +0300

    radio button works

--- lesson_test_8_1.py
+++ lesson_test_8_1.py
@@ -8,9 +8,9 @@ from selenium.webdriver.common.by import By
 # options.add_experimental_option("detach", True)
 # g = Service()
 # driver = webdriver.Chrome(options=options, service=g)  # this is and above macOS
-driver = webdriver.Chrome()  # Windows
+# driver = webdriver.Chrome()  # Windows
 # driver = webdriver.Firefox()
-# driver = webdriver.Safari()
+driver = webdriver.Safari()
 # driver = webdriver.Edge()
 base_url = 'https://demoqa.com/radio-button'
 driver.get(base_url)
@@ -24,6 +24,9 @@ driver.maximize_window()
 # check_box_1.click()
 radio_button = driver.find_element(By.XPATH, "//label[@for='yesRadio']")
 radio_button.click()
+value_radio_button = radio_button.text
+assert value_radio_button == "Yes"
+print("PASSED")
 
 
 
