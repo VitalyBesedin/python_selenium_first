bf0d89dd3e989c2b09bec45c962f18c7775345d9
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-27 22:50:40 +0300

    exceptions finish

--- lesson_test_8_4.py
+++ lesson_test_8_4.py
@@ -14,38 +14,39 @@ driver = webdriver.Chrome(options=options, service=g)  # this is and above macOS
 # driver = webdriver.Firefox()
 # driver = webdriver.Safari()
 # driver = webdriver.Edge()
-base_url = 'https://demoqa.com/dynamic-properties'
+base_url = 'https://demoqa.com/radio-button'
 driver.get(base_url)
 driver.maximize_window()
 
-# new_date = driver.find_element(By.XPATH, "//input[@id='datePickerMonthYearInput']")
-# new_date.click()
-# now_date = datetime.datetime.utcnow().strftime("%d")
-# print(now_date)
-# date = int(now_date) + 1
-# locator = "//div[@aria-label='Choose Tuesday, May " + str(date) + "rd, 2023']"
-# print(locator)
-#
-# time.sleep(3)
-# date_23 = driver.find_element(By.XPATH, locator)
-# date_23 = driver.find_element(By.XPATH, "//div[@aria-label='Choose Tuesday, May 23rd, 2023']")
-# date_23.click()
-# new_date.clear()
-# for _ in range(10):
-#     new_date.send_keys(Keys.BACKSPACE)
-# time.sleep(3)
-# new_date.send_keys("03/13/2023")
-# time.sleep(3)
-# new_date.send_keys(Keys.RETURN)
 
+# try:
+#     visible_button = driver.find_element(By.CSS_SELECTOR, "#visibleAfter")
+#     visible_button.click()
+# except NoSuchElementException as exception:
+#     print("NoSuchElementException exception")
+#     time.sleep(7)
+#     visible_button = driver.find_element(By.CSS_SELECTOR, "#visibleAfter")
+#     visible_button.click()
+#     print("Click Visible Button")
+
+yes_checkbox = driver.find_element(By.XPATH, "//label[@for='yesRadio']")
+yes_checkbox.click()
 try:
-    visible_button = driver.find_element(By.CSS_SELECTOR, "#visibleAfter")
-    visible_button.click()
-except NoSuchElementException as exception:
-    print("NoSuchElementException exception")
-    time.sleep(7)
-    visible_button = driver.find_element(By.CSS_SELECTOR, "#visibleAfter")
-    visible_button.click()
-    print("Click Visible Button")
+    message = driver.find_element(By.XPATH, "//span[@class='text-success']")
+    value_message = message.text
+    print(value_message)
+    assert value_message == 'No'
+    print('checkbox No')
+except AssertionError as exception:
+    driver.refresh()
+    yes_checkbox = driver.find_element(By.XPATH, "//label[@for='yesRadio']")
+    yes_checkbox.click()
+    message = driver.find_element(By.XPATH, "//span[@class='text-success']")
+    value_message = message.text
+    print(value_message)
+    assert value_message == 'Yes'
+    print('checkbox Yes')
+print('test over')
 
-time.sleep(10)
+
+# time.sleep(10)
